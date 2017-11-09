01fbd30c1bbc2a70e153c344f7634bd2a1ccc905
Jonathan <jonathann.maimon@gmail.com>
2017-11-09 09:10:50 +0200

    updating back button

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -230,6 +230,8 @@ function createButtons(index) {
         button.addEventListener ("click", function() {
             var temp = Next[index][this.id];
             var tempArr = [index, this.id];
+            if(index == 0)
+                document.getElementById("backButton").style = "display: block;"             
             console.log('temp'+this.id);
             // document.getElementsByClassName("toolbar")[0].style = "display: block;"
             Back.push(tempArr);      
